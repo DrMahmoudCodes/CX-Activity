e2b47c8923ccb8ae4508dba07b2e1fb8078038bd
DrMahmoudCodes <mahmoudhanwer@gmail.com>
2025-12-20 18:14:46 +0200

    CX

---
+++ CX-Activity V2/CX-Activity SemiFinal V2/story_content/triggers.js
@@ -0,0 +1,31 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6dhuF1Lp6Yi":
+        Script1();
+        break;
+      case "5V5tvnbjS1t":
+        Script2();
+        break;
+      case "5hHOEbRLbkT":
+        Script3();
+        break;
+      case "6hW0129mFnL":
+        Script4();
+        break;
+      case "5VZrxH2kxlo":
+        Script5();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
